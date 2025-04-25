9b4512917951ea0c8765cfa88b4f909a3114c690
Zarahhhh <zakariyyahrahmat@gmail.com>
2025-04-25 15:33:54 +0100

    Completed ES6 Basics project

---
+++ 100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  const all = [];
+  for (const department of Object.values(report.allEmployees)) {
+    all.push(...department);
+  }
+
+  return all;
+}
+
---
+++ 9-getFullBudget.js
@@ -0,0 +1,10 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  return {
+    ...budget,
+    getIncome: () => budget.income,
+  };
+}
+
